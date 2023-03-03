94af11e35e78a3c4b71e4b0e6ae7820a974d5de2
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2023-03-03 11:56:31 -0500

    fix logic

--- helper/helper.js
+++ helper/helper.js
@@ -97,16 +97,31 @@ const solver = (board) => {
 
 function validSudoku(board) {
     for (let i = 0; i < 9; i++) {
+        let row = new Set(),
+            col = new Set(),
+            box = new Set();
+
         for (let j = 0; j < 9; j++) {
-            const value = board[i][j];
-            if (value !== 0) {
-                if (!checkRow(board, i, value) || !checkColumn(board, j, value) || !checkSquare(board, i, j, value)) {
-                    return false;
-                }
+            let _row = board[i][j];
+            let _col = board[j][i];
+            let _box = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
+
+            if (_row != 0) {
+                if (row.has(_row)) return false;
+                row.add(_row);
+            }
+            if (_col != 0) {
+                if (col.has(_col)) return false;
+                col.add(_col);
+            }
+
+            if (_box != 0) {
+                if (box.has(_box)) return false;
+                box.add(_box);
             }
         }
     }
-    return true;
+    return true
 }
 
 module.exports = { randomBoard, getTodaysDate, solver, validSudoku }
